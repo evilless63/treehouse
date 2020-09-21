7664272c27ad28238adf30097da1f8c265392715
vagrant <vagrant@homestead>
2020-09-21 20:56:25 +0000

    adding plugins

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,14 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+
 class Category extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     protected $primaryKey = 'category_id';
     protected $keyType = 'string';
     protected $guarded = '';
     public $incrementing = false;
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 
     // Выгружаем из responce json (из тз 1с) в бд, проверяем изменения. Если надо добавляем новые записи.
 }
