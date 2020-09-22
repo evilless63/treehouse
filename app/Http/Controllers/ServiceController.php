d0fc008bd111dbc520b70fa50f72f009d3fb298b
vagrant <vagrant@homestead>
2020-09-22 09:11:55 +0000

    api categories

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -20,6 +20,7 @@ class ServiceController extends Controller
                     $category = Category::where('category_id', $s['category_id'])->first();
                     $transliterator = new Transliterator(Map::LANG_RU, Map::GOST_7_79_2000);
                     $s['name-translite'] = $transliterator->make($s['name']);
+                    Log::info($s);
                     if($category == null) {
                         $category = new Category();
                         $category->save($s);
