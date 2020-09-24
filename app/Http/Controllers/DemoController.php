bca6bb3ecf062a8f49b92842869219490e4f3b48
vagrant <vagrant@homestead>
2020-09-24 15:50:22 +0000

    request to 1c from controller

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
 
 class DemoController extends Controller
 {
@@ -14,6 +18,36 @@ class DemoController extends Controller
     }
 
     public function postOrderTo1c(Request $request) {
-        dd($request);
+        $client = new Client();
+
+        // $promise = $client->requestAsync('POST', 'http://31.128.156.218:55315/ushp/hs/obmen/get-orders', [
+        //     'auth' => ['Анна', '17382256Ksu@'],
+        //     'body' => $request->product_id
+        // ]);
+        // $promise->then(
+        //     function (ResponseInterface $res) {
+        //         Log::info($res->getStatusCode());
+        //     },
+        //     function (RequestException $e) {
+        //         Log::info($e->getMessage());
+        //         Log::info($e->getRequest()->getMethod());
+        //     }
+        // );
+        try {
+            $response = $client->request('POST', 'http://31.128.156.218:55315/ushp/hs/obmen/get-orders', [
+                'auth' => ['Анна', '17382256Ksu@'],
+                'body' => $request->product_id
+            ]);
+        } catch (Exception $e) {
+            Log::info($e); 
+            return response('ERROR', 500);
+        }
+
+        $body = $response->getBody();
+        $stringBody = (string) $body;
+
+        Log::info($response->getStatusCode());
+        Log::info($stringBody);
+        // $response = $client->request('POST', 'test');
     }
 }
